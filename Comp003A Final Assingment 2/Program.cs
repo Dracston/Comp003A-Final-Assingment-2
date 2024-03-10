02f29b8b21ab9e01e2396cd09ab821cf9e13f860
Logan Jones <lpj11101@gmail.com>
2024-03-10 08:31:08 -0700

    Working Copy

--- Comp003A Final Assingment 2/Program.cs
+++ Comp003A Final Assingment 2/Program.cs
@@ -11,32 +11,35 @@ namespace Comp003A_Final_Assingment_2
         static void Main(string[] args)
         {
             List<string> questionarre = new List<string>(new string[] {
-                "What is your First name?", //1
-                "What is your Last name?", //2
-                "What year were you born in?", //3 
-                "Are you Male (M), Female (F), or another option (O)?", //4
-                "How tall are you (In Inches)?", //5
-                "On a scale from 1-10, what is your combat experience?", //6 
-                "Are you willing to put your life on the line for Super Earth?\n(Yes/No)", //7 
-                "What is your shoe size?", //8
-                "In the event of a horific, sudden, potential, and unavoidable death by galactic entity, can your body be used for science?\n(Yes/No)", //9
-                "How fast can you run 100 meters (In seconds)?", //10
-                "Do you have previous Super Earth regulated firearm training?\n(Yes/No)", //11
-                "Have you or a loved one been previously diagnosed with death by Terminid mutilation?\n(Yes/No)", //12
-                "How much weight (In pounds) do you believe you could carry for 100 meters continously?", //13
-                "Will you be needing any additional medical treatments/ supplies?\n(Yes/No)", //14
-                "Which body part would you least want rennovated with bionic enhancements?"}); //15
-            List<string> userAnswers = new List<string>();
+                "What is your First name?", //0
+                "What is your Last name?", //1
+                "What year were you born in?", //2
+                "Are you Male (M), Female (F), or another option (O)?", //3
+                "How tall are you (In Inches)?", //4
+                "On a scale from 1-10, what is your combat experience?", //5
+                "Are you willing to put your life on the line for Super Earth?\n(Yes/No)", //6
+                "What is your shoe size?", //7
+                "In the event of a horific, sudden, potential, and unavoidable death by galactic entity, can your body be used for science?\n(Yes/No)", //8
+                "How fast can you run 100 meters (In seconds)?", //9
+                "Do you have previous Super Earth regulated firearm training?\n(Yes/No)", //10
+                "Have you or a loved one been previously diagnosed with death by Terminid mutilation?\n(Yes/No)", //11
+                "How much weight (In pounds) do you believe you could carry for 100 meters continously?", //12
+                "Will you be needing any additional medical treatments/ supplies?\n(Yes/No)", //13
+                "Which body part would you least want rennovated with bionic enhancements?"}); //14
+            List<string> userAnswers = new List<string>(); //This empty list will hold all of the answers given
 
 
 
-
+            ///<summary> 
+            ///Each question was written individually, as each question needed different validation types
+            ///
+            /// /summary>
             Console.WriteLine("Hello, and welcome to Helldiver enlistment! Below are a few short questions we need you to answer truthfully.");
 
-            Console.WriteLine($"{questionarre[0]}");
-            string firstName = Console.ReadLine();
-            StringValidation(firstName);
-            userAnswers.Add(firstName);
+            Console.WriteLine($"{questionarre[0]}");  ///<param> Write the question out
+            string firstName = Console.ReadLine();     /// Take a unique variable for the question and set it to ReadLine
+            StringValidation(firstName);               /// Call the appropriate method. Either needing to validate a string or integer. Or two specialized ones
+            userAnswers.Add(firstName);                /// Then save the returned string to the array of userAnswers[] </param>
 
             Console.WriteLine($"{questionarre[1]}");
             string lastName = Console.ReadLine();
@@ -73,8 +76,8 @@ namespace Comp003A_Final_Assingment_2
             StringValidation(questionSevenAnswer);
             userAnswers.Add(questionSevenAnswer);
 
-            Console.WriteLine($"{questionarre[7]}");
-            string shoeSize = Console.ReadLine();
+            Console.WriteLine($"{questionarre[7]}");  ///<summary> Because HeightValidation allows for any int between 1-3 digits. O re-use it for all int validations.
+            string shoeSize = Console.ReadLine();     /// I just found it simple to keep the name as is. But HeightValidation = Validator for ant 1-3 digit number </summary>
             HeightValidation(shoeSize);
             userAnswers.Add(shoeSize);
 
@@ -115,14 +118,14 @@ namespace Comp003A_Final_Assingment_2
 
 
 
-          
 
-           for (int i = 0; i < questionarre.Count; i++)
+            ///<summary>First while loop states the user's full name after it has been given. Second while loop stops the main for loop to write the user's age. 
+            for (int i = 0; i < questionarre.Count; i++)
             {
                 while (i == 2)
                 {
                     Console.WriteLine($"Full Name: {userAnswers[0]} {userAnswers[1]}.");
-                    break;
+                    break;//break to avoid stackoverflow
                 }
                  
                 
@@ -130,7 +133,7 @@ namespace Comp003A_Final_Assingment_2
                 {
                     
                     Console.WriteLine($"Current age is: {DateTime.Now.Year - CreateInt(birthYear)}");
-                    break;
+                    break; //break to avoid stackoverflow
                     
                 }
                
@@ -162,7 +165,7 @@ namespace Comp003A_Final_Assingment_2
         }
        
         static string HeightValidation(string userHeight) 
-            ///<summary> Validate my Int for asking someone's height</summary>
+            ///<summary> Validate my Int for asking someone's height/ any 1-3 digit number </summary>
         {
             if (CheckInt(userHeight))
             {
@@ -192,7 +195,7 @@ namespace Comp003A_Final_Assingment_2
         }
         
         static string YearValidation(string birthYear)
-        {
+        { ///<summary> Validates whether the int added is between certain years. 
             
             if (CheckInt(birthYear))
             {
@@ -200,7 +203,7 @@ namespace Comp003A_Final_Assingment_2
 
                 if (userYear < 1900)
                 {
-                    Console.WriteLine("Please re-enter birth year. You're not that old.");
+                    Console.WriteLine("Please re-enter birth year. You're not that old."); //flavor text error message
                     birthYear = Console.ReadLine();
                     YearValidation(birthYear);
                 }
@@ -216,7 +219,7 @@ namespace Comp003A_Final_Assingment_2
                     
                 }
             }
-            else 
+            else //Checks here to make sure no null/ string values are given
             {
                 Console.WriteLine("Try that one more time, I need a number!");
                 birthYear = Console.ReadLine();
@@ -226,7 +229,9 @@ namespace Comp003A_Final_Assingment_2
         }
 
         static string StringValidation(string userInput)
-        {
+        { 
+            ///<summary>Simple Try.Parse, but code cluster only moves forward if the check is false
+            ///</summary>
             
                 if (CheckInt(userInput))
                 {
@@ -239,7 +244,7 @@ namespace Comp003A_Final_Assingment_2
 
                 }
                 
-                if(userInput.Length == 0)
+                if(userInput.Length == 0) //Secondary check to avoid Null values and re-loop statement
             {
                 Console.WriteLine("Please answer the question.");
                 userInput= Console.ReadLine();
@@ -249,12 +254,15 @@ namespace Comp003A_Final_Assingment_2
         }
 
         static string GenderValidation(char userInput)
-        {
+        { 
+            ///<summary>
+            ///Makes the user only able to input one char. Changes that char into a string for userAnswers list. 
+            /// </summary>
             
             
                 userInput= char.ToUpper(userInput);
             
-                if (userInput == 'M')
+                if (userInput == 'M') // If statements changes the characters into their full length word and returns one. 
                 {
                    string genderAnswer ="Male";
                     return genderAnswer; 
@@ -273,7 +281,7 @@ namespace Comp003A_Final_Assingment_2
                 else 
                 {
                 Console.WriteLine("Please select 'M', 'F', or 'O'");
-                return userInput.ToString();
+                return userInput.ToString(); //This is mostly here so the Method feels like it will always have a return value occurring. 
                 userInput =Convert.ToChar(Console.ReadLine());
                 GenderValidation(userInput);
             
